7eef86fbede5c6df878393a225399663c340f26e
gpro19 <119834015+gpro19@users.noreply.github.com>
2025-02-01 05:27:26 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -168,7 +168,7 @@ def index():
 
 
 def run_flask():
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8000)))
 
 
 def main():
