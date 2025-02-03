ed4bcf27a3033c4ed9b9d72a16dc750dff4131d2
gpro19 <119834015+gpro19@users.noreply.github.com>
2025-02-03 16:54:13 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, CallbackQueryHandler
 import time
 import re
 from flask import Flask, jsonify
